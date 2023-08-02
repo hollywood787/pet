212093166235c4404cd2aa8b96641bed9604f002
hollywood787 <hollywood7878@yandex.ru>
2023-08-02 18:50:56 +0300

    add svg icons

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,6 +1,7 @@
 import { classNames } from "shared/lib/classNames/classNames";
 import cls from "./Navbar.module.scss";
 import { AppLink, AppLinkTheme } from "shared/ui/AppLink/AppLink";
+import { ThemeSwitcher } from "../../../shared/ui/ThemeSwitcher";
 
 interface NavbarProps {
   className?: string;
@@ -9,9 +10,18 @@ interface NavbarProps {
 export const Navbar = ({ className }: NavbarProps) => {
   return (
     <div className={classNames(cls.Navbar, {}, [className])}>
+      <ThemeSwitcher />
       <div className={cls.links}>
-        <AppLink to={"/"} theme={AppLinkTheme.SECONDARY} className={cls.mainLink}>Главная</AppLink>
-        <AppLink to={"/about"} theme={AppLinkTheme.SECONDARY}>О сайте</AppLink>
+        <AppLink
+          to={"/"}
+          theme={AppLinkTheme.SECONDARY}
+          className={cls.mainLink}
+        >
+          Главная
+        </AppLink>
+        <AppLink to={"/about"} theme={AppLinkTheme.SECONDARY}>
+          О сайте
+        </AppLink>
       </div>
     </div>
   );
