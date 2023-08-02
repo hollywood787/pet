3d1ccca0bb6a2df86bd158cc7f16ca7722b352d2
hollywood787 <hollywood7878@yandex.ru>
2023-08-02 23:13:23 +0300

    add i18n and translate button

---
+++ src/shared/config/i18n/i18n.ts
@@ -0,0 +1,18 @@
+import i18n from 'i18next';
+import { initReactI18next } from "react-i18next";
+
+import Backend from 'i18next-http-backend';
+import LanguageDtector from 'i18next-browser-languagedetector';
+
+i18n
+    .use(Backend)
+    .use(LanguageDtector)
+    .use(initReactI18next)
+    .init({
+        fallbackLng: 'ru',
+        debug: __IS_DEV__ ? true : false,
+
+        interpolation: {
+            escapeValue: false,
+        }
+    });
